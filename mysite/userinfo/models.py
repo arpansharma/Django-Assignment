448aaea0bbdb8d20cedeade52b1d84af95a51fac
arpansharma <arpan.sharma011@gmail.com>
2018-06-19 12:14:46 +0530

    Updated models

--- mysite/userinfo/models.py
+++ mysite/userinfo/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 class UserInformation(models.Model):
-	user_id = models.AutoField(primary_key=True)
+	usr_nm = models.CharField(max_length=30)
 	user_name = models.CharField(max_length=30)
 	location = models.CharField(max_length=250)
 	public_gists_count = models.PositiveIntegerField()
 	public_repos_count = models.PositiveIntegerField()
+	created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+	def __str__(self):
+		return self.usr_nm
 
 class RepoInformation(models.Model):
 	repo_id = models.PositiveIntegerField()

e4e960c04b686b2a1585ca3b8856f2be0ecc00cc
arpansharma <arpan.sharma011@gmail.com>
2018-06-19 20:50:59 +0530

    Updated views.py

--- mysite/userinfo/views.py
+++ mysite/userinfo/views.py
@@ -4,41 +4,49 @@ from .models import UserInformation
 
 def index(request):
 
-	userDataObject = {}
-	if request.method == 'GET':
-		username = request.GET.get('usrname')
-		if not username:
-			return render(request, 'index.html')
-		else:
-			userDataObject = UserInformation.objects.filter(username__icontains=username)
-			return render(request, 'index.html', userDataObject)
+    userDataObject = {}
+    if request.method == 'GET':
+        username = request.GET.get('usrname')
+        if not username:
+            return render(request, 'index.html')
 
-	if request.method == 'POST':
-		print ("Hurrah!")
-		username = request.POST.get('usrname')
-		userInfoUrl = 'https://api.github.com/users/' + username
-		responseUserInfo = requests.get(userInfoUrl).json()
+    elif request.method == 'POST':
+        username = request.POST.get('usrname')
 
-		#Getting info from API
-		userInfo = {
-		'usrnm' : username,
-		'name' : responseUserInfo['name'],
-		'location' : responseUserInfo['location'],
-		'publicGistsCount' : responseUserInfo['public_gists'],
-		'publicReposCount' : responseUserInfo['public_repos'],
-		}
+        #returns a query set
+        userinformation = UserInformation.objects.filter(usr_nm__iexact=username)
+        if not userinformation:
+            userInfoUrl = 'https://api.github.com/users/' + username
+            responseUserInfo = requests.get(userInfoUrl)
+            #converted using request json method
+            if responseUserInfo.status_code == 200:
+                responseUserInfo = responseUserInfo.json()
+            else:
+                userData = {'error' :  "No such user found!"}
+                return render(request, 'index.html', userData)
 
-		#Saving to the database
-		userinformation = UserInformation(
-			usr_nm = userInfo['usrnm'],
-			user_name = userInfo['name'],
-			location = userInfo['location'],
-			public_gists_count = userInfo['publicGistsCount'],
-			public_repos_count = userInfo['publicReposCount'])
-		
-		userinformation.save()
+            #Getting info from API
+            userInfo = {
+            'usrnm' : username,
+            'name' : responseUserInfo['name'],
+            'location' : responseUserInfo['location'],
+            'publicGistsCount' : responseUserInfo['public_gists'],
+            'publicReposCount' : responseUserInfo['public_repos'],
+            'followers' : responseUserInfo['followers'],
+            'following' : responseUserInfo['following'],
+            }
 
-		userDataObject = {'usr_info' : userInfo}
-		print (userDataObject)	
-		return render(request, "index.html", userDataObject)
-		#return render(request, "/?username=userInfo['usrnm']")
+            #Saving to the database
+            userinformation = UserInformation(
+                usr_nm = userInfo['usrnm'],
+                user_name = userInfo['name'],
+                location = userInfo['location'],
+                public_gists_count = userInfo['publicGistsCount'],
+                public_repos_count = userInfo['publicReposCount'])
+            
+            userinformation.save()
+        else:
+            userinformation = userinformation[0]
+
+        userData = {'usr_info' : userinformation.__dict__}
+        return render(request, 'index.html', userData)

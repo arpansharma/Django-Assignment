b94daffde9b0d8997ee16fc74a9f31ac3736b731
arpansharma <arpan.sharma011@gmail.com>
2018-06-23 02:35:38 +0530

    Added URL for branch details

--- mysite/userinfo/urls.py
+++ mysite/userinfo/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('repo/', views.repo, name='repo'),
-	path('', views.branch, name='branch'),
+	path('repo/branch/', views.branch, name='branch'),
 	path('', views.commit, name='commit'),
 ]
